e50e59a5a6d68d1396434129e4cc671309cac54b
dagt07 <davidgarciat07@gmail.com>
2024-05-13 13:20:52 -0400

    FIX: Solve conflict in urls.py

--- aprende_beauchef/baseapp/urls.py
+++ aprende_beauchef/baseapp/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from baseapp import views
 
-urlpatterns =  [
+urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login, name="login"),
-<<<<<<< HEAD
     path("publicar/", views.publicar, name="publicar"),
-=======
     path("register/", views.register, name="register"),
->>>>>>> 31c44294a46444807fd89463589d4931cf1aa2d9
-    ]
+]
