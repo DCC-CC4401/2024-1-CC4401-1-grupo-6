5328860474fcb9a527fad7864e6bf71f4115771b
dagt07 <davidgarciat07@gmail.com>
2024-05-17 14:29:21 -0400

    FEAT: add templates for test GET and POST methods

--- aprende_beauchef/baseapp/views.py
+++ aprende_beauchef/baseapp/views.py
@@ -16,3 +16,11 @@ def publicar(request):
 
 def register(request):
     return render(request, "register.html")
+
+
+def test(request):
+    if request.method == "GET":
+        return render(request, "test_form.html")
+    if request.method == "POST":
+        data = request.POST
+        return render(request, "test_items.html", {"data": data})
