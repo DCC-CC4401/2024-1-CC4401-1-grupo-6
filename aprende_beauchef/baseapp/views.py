d8a1692a0651466ae101984a2f6efb45f6f9a6f5
Lucero3412 <lucero2003.martina@gmail.com>
2024-05-19 16:15:48 -0400

    DOCS: update views.py with methods's explanations

--- aprende_beauchef/baseapp/views.py
+++ aprende_beauchef/baseapp/views.py
@@ -4,11 +4,24 @@ from .models import Usuario, Tutor, Estudiante
 from django.contrib.auth import logout
 
 
-# Create your views here.
+"""
+Renderiza la página principal de la aplicación
+Usa el método render que construye la plantilla index
+Como requiere información de la base de datos, accede para extraer afiches y mostrarlos
+
+parámetro request Información relacionada a la solicitud que se realiza
+"""
 def index(request):
     return render(request, "index.html")
 
+"""
+Dependiendo el método, renderiza la página de acceso a la aplicación o permite el acceso.
+Si la solicitud es un GET, construye la plantilla login. Si es un POST, revisa que la
+información entregada corresponda a un usuario registrado en la página. Si es así,
+permite ingresar en la sesión y redireacciona a la página principal. Sino muestra un error. 
 
+parámetro request Información relacionada a la solicitud que se realiza, puede ser un GET o un POST
+"""
 def login_view(request):
     if request.method == "GET":
         return render(request, "login.html")
@@ -24,16 +37,36 @@ def login_view(request):
                 request, "login.html", {"error": "Invalid username or password"}
             )
 
+"""
+Cierra la sesión de un usuario autentificado en la aplicación
+Usa el método logout para limpiar la información relacionada a la sesión del usuario. 
+No retorna. Luego se redirecciona a la página principal con el método redirect.
 
+parámetro request Información relacionada a la solicitud que se realiza
+"""
 def logout_user(request):
     logout(request)
     return redirect("index")
 
+"""
+Renderiza la página relacionada a la publicación de afiches
+Usa el método render que construye la plantilla publicar
 
+parámetro request Información relacionada a la solicitud que se realiza
+"""
 def publish(request):
     return render(request, "publicar.html")
 
+"""
+Dependiendo del método, renderiza la página de registro o ingresa la información 
+de un usuario nuevo a la base de datos.
+Si la solicitud es un GET, construye la plantilla register. Si es un POST, recibe
+la información enviada por el formulario de registro y crea un nuevo usuario y, en su defecto,
+un estudiante. Luego redirecciona a la página login para que el usuario pueda ingresar.
 
+parámetro request Información relacionada a la solicitud que se realiza, puede ser un GET o un POST
+
+"""
 def register(request):
     if request.method == "GET":
         return render(request, "register.html")
