c7339863839032af526c2ef5cac592556713a13f
Luis Marin <luis.marin.t@ug.uchile.cl>
2024-05-21 20:45:23 -0400

    FIX: error upload css in index

--- aprende_beauchef/aprende_beauchef/settings.py
+++ aprende_beauchef/aprende_beauchef/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/aprende_beauchef/baseapp/static/"
+STATIC_URL = "/static/"
 MEDIA_URL = "/aprende_beauchef/baseapp/uploads/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "baseapp/uploads")
 
