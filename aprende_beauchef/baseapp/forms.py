f3a47486c56e586090bd1994df3bb96974623465
Marcelo <marcelo.chuang@ug.uchile.cl>
2024-06-16 19:00:24 -0400

    BUILD: create LoginRecoveryPassword form and LoginNewPassword form

---
+++ aprende_beauchef/baseapp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+
+
+# Login Form #
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Nombre de usuario", max_length=25,
+                            widget=forms.TextInput(attrs={'class': 'username_input','id': 'username'}), 
+                            required=True, error_messages={'required': 'Este campo es obligatorio'})
+    password = forms.CharField(label="Contraseña", 
+                            widget=forms.PasswordInput(attrs={'class': 'password_input','id': 'password'}), 
+                            required=True, error_messages={'required': 'Este campo es obligatorio'})
+
+class LoginRecoveryPassword(forms.Form):
+    email = forms.EmailField(label="Correo electrónico", max_length=50, 
+                            widget=forms.TextInput(attrs={'class': 'email','id': 'username'}), 
+                            error_messages={'required': 'Este campo es obligatorio'}, required=True)
+
+class LoginNewPassword(forms.Form):
+    password = forms.CharField(label="Contraseña", 
+                            widget=forms.PasswordInput(attrs={'class': 'password_input','id': 'password'}), 
+                            required=True, error_messages={'required': 'Este campo es obligatorio'})
+    confirmPassword = forms.CharField(label="Confirmar contraseña", 
+                                    widget=forms.PasswordInput(attrs={'class': 'password_input','id': 'password'}), 
+                                    required=True, error_messages={'required': 'Este campo es obligatorio'})
--- aprende_beauchef/baseapp/urls.py
+++ aprende_beauchef/baseapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path("logout/", views.logout_user, name="logout"),
     path("publicar/", views.publish, name="publicar"),
     path("register/", views.register, name="register"),
+    path("restablecer_contraseña/", views.reset_password, name="restablecer_contraseña"),
+    path("nueva_contraseña/", views.new_password, name="nueva_contraseña"),
 ]
 
 if settings.DEBUG:
