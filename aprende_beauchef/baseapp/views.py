a8344edb226b18cb90ced43153f8a265f8b977f4
Vichito22 <vicente.leyton.c@ug.uchile.cl>
2024-06-21 10:05:35 -0400

    FEAT: add modality filter

--- aprende_beauchef/baseapp/views.py
+++ aprende_beauchef/baseapp/views.py
@@ -49,6 +49,8 @@ def index(request):
                 publicaciones = publicaciones.filter(publica__dicta__tutor__precio__gte=min_price).order_by('-id')
             if alldisponibility != 'ALL':
                 publicaciones = publicaciones.filter(publica__dicta__tutor__horario__dia_semana=disponibility).order_by('-id')
+            if modality != 'AMB':
+                publicaciones = publicaciones.filter(publica__dicta__tutor__modalidad_preferida=modality).order_by('-id')
             
             #La idea es que sigan filtrando de esta forma, es decir, reasignando publicaciones
             #con los filtros para modalidad.
